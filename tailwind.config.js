06c258495bc0b04b3047015c16fce96df7a23109
Rasme Akter <87582286+raakstmeer@users.noreply.github.com>
2022-09-14 10:47:40 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./account.html','./login.html','./profileinfo.html','./shop.html','./signup.html','./view.html','./wishlist.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
